0560ab7de6371b355ab2631b00aeb19a3258a35b
Anastasia <kazantseva_nastya_95@mail.ru>
2022-10-18 16:04:40 +0500

    Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,3 +3,25 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+  int SumNumber(int num)
+  {
+    
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < num; i++){
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
